f140d231cd01bf09448600ef0a94f26642d000f2
Anthony Paz <douglas.paz@unah.hn>
2025-01-22 23:55:41 -0600

    Create Prueba5.js

---
+++ Prueba5.js
@@ -0,0 +1,8 @@
+// Douglas Anthony Paz Hernandez
+
+const texto = prompt('Ingrese un texto: ');
+
+const textoInvertido = texto.split('').reverse().join('');
+
+// Resultado
+console.log(textoInvertido);
